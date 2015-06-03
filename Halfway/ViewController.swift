4754827ce33a985cc9cb58f445d46c0633f6fb7f
mitasray <archerkinggod@gmail.com>
2015-06-03 14:59:08 -0400

    Initial Commit

---
+++ Halfway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Halfway
+//
+//  Created by Mitas Ray on 6/3/15.
+//  Copyright (c) 2015 mitas.ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

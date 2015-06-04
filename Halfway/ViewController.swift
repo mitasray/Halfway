389eaa646c73b8902d15d19e49d62eb3ce89e16b
mitasray <archerkinggod@gmail.com>
2015-06-04 17:36:48 -0400

    extract HTML for input address

--- Halfway/ViewController.swift
+++ Halfway/ViewController.swift
@@ -16,7 +16,39 @@ class ViewController: UIViewController {
     @IBOutlet var state : UITextField!
     
     @IBAction func donePressed(sender: AnyObject) {
-        var fullAddress = address.text + ", " + city.text + ", " + state.text
+        var fullAddress = address.text + " " + city.text + " " + state.text
+        // println(fullAddress)
+        findLatLong(fullAddress)
+    }
+    
+    /**
+     * http://stackoverflow.com/questions/26134884/how-to-get-html-source-from-url-with-swift
+     * Returns the HTML source code of the geocoder.us webpage with the proper address.
+     */
+    func getHTML(fullAddress : String) -> NSString {
+        var urlAddress = fullAddress.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        
+        let myURLString = "http://geocoder.us/demo.cgi?address=" + urlAddress
+        
+        if let myURL = NSURL(string: myURLString) {
+            var error: NSError?
+            let myHTMLString = NSString(contentsOfURL: myURL, encoding: NSUTF8StringEncoding, error: &error)
+            
+            if let error = error {
+                println("Error : \(error)")
+            } else {
+                println("HTML : \(myHTMLString)")
+                return myHTMLString!
+            }
+        } else {
+            println("Error: \(myURLString) doesn't seem to be a valid URL")
+        }
+        return NSString(string: "")
+    }
+    
+    func findLatLong(fullAddress : String) {
+        var HTMLString = getHTML(fullAddress)
+        
     }
     
     @IBAction func viewTapped(sender: AnyObject) {

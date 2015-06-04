dbba81f344f627461d64e2956b62d796069885da
mitasray <archerkinggod@gmail.com>
2015-06-04 01:14:09 -0400

    address can be inputted and saved into variable

--- Halfway/ViewController.swift
+++ Halfway/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+// Map Tutorial: http://www.raywenderlich.com/90971/introduction-mapkit-swift-tutorial
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var address : UITextField!
+    @IBOutlet var city : UITextField!
+    @IBOutlet var state : UITextField!
+    
+    @IBAction func donePressed(sender: AnyObject) {
+        var fullAddress = address.text + ", " + city.text + ", " + state.text
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        // Empty.
+    }
+    
+    // Getting HTML source code from url: http://stackoverflow.com/questions/26134884/how-to-get-html-source-from-url-with-swift
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 

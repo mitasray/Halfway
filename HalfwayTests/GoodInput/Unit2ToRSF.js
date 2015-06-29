697b4b01046f8ad5a7fd4343038e9fa61cbbcd00
mitasray <mitas.ray@gmail.com>
2015-06-28 20:59:46 -0400

    NCMitasToNIEHS.js removed because system location changes were unstable

--- HalfwayTests/GoodInput/Unit2ToRSF.js
+++ HalfwayTests/GoodInput/Unit2ToRSF.js
@@ -4,42 +4,39 @@
  * should be Top Dog, 2534 Durant Ave.
  */
 
-function Unit2ToRSF() {
-	var target = UIATarget.localTarget();
-	var coordinates = {}
-	coordinates["latitude"] = 37.866287
-	coordinates["longitude"] = -122.255251
-	target.setLocation(coordinates)
+var target = UIATarget.localTarget();
+var coordinates = {};
+coordinates["latitude"] = 37.866287;
+coordinates["longitude"] = -122.255251;
+target.setLocation(coordinates);
 
-	var mainWindow = target.frontMostApp().mainWindow();
+var mainWindow = target.frontMostApp().mainWindow();
 
-	var friendAddress = mainWindow.textFields()[0];
-	var friendCity = mainWindow.textFields()[1];
-	var friendState = mainWindow.textFields()[2];
+var friendAddress = mainWindow.textFields()[0];
+var friendCity = mainWindow.textFields()[1];
+var friendState = mainWindow.textFields()[2];
 
-	friendAddress.setValue("2301 Bancroft Way");
-	friendCity.setValue("Berkeley");
-	friendState.setValue("CA");
+friendAddress.setValue("2301 Bancroft Way");
+friendCity.setValue("Berkeley");
+friendState.setValue("CA");
 
-	var halfwayButton = mainWindow.buttons()[0];
-	halfwayButton.tap();
-	target.delay(2)
-	halfwayButton.tap(); // This second tap is required because of the closure error.
+var halfwayButton = mainWindow.buttons()[0];
+halfwayButton.tap();
+target.delay(2)
+halfwayButton.tap(); // This second tap is required because of the closure error.
 
-	var resultLocation = mainWindow.staticTexts()[2];
-	var resultAddress = mainWindow.staticTexts()[4];
+var resultLocation = mainWindow.staticTexts()[2];
+var resultAddress = mainWindow.staticTexts()[4];
 
-	if (resultLocation.value() == "Top Dog") {
-	    UIALogger.logPass("Result Location is CORRECT.");
-	} else {
-	    UIALogger.logFail("Result Location is INCORRECT.");
-	}
-
-	if (resultAddress.value() == "2534 Durant Ave") {
-	    UIALogger.logPass("Result Address is CORRECT.");
-	} else {
-	    UIALogger.logFail("Result Address is INCORRECT.");
-	}
+if (resultLocation.value() == "Top Dog") {
+    UIALogger.logPass("Result Location is CORRECT.");
+} else {
+    UIALogger.logFail("Result Location is INCORRECT.");
 }
 
-Unit2ToRSF()
+if (resultAddress.value() == "2534 Durant Ave") {
+    UIALogger.logPass("Result Address is CORRECT.");
+} else {
+    UIALogger.logFail("Result Address is INCORRECT.");
+}
+

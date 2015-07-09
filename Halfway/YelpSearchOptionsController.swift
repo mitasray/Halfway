51e459be957a4bbd185fed5c01a78c4d53b30117
mitasray <mitas.ray@gmail.com>
2015-07-09 17:09:47 -0400

    created tableview but weird error asking for nib registration

--- Halfway/YelpSearchOptionsController.swift
+++ Halfway/YelpSearchOptionsController.swift
@@ -10,5 +10,21 @@ import Foundation
 import UIKit
 
 public class YelpSearchOptionsController: UITableViewController {
-
+    
+    var options: [String] = ["Restaurant", "Coffee", "Bar", "Park", "Mall"]
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    public override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        var option: String = options[indexPath.row]
+        cell.textLabel?.text = option
+        return cell
+    }
 }
